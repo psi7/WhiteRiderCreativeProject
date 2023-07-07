b50fb6f2fe7feb67b47cac3fdef70b89cdaf7d48
psi7 <pansid7@gmail.com>
2023-07-07 13:57:57 +0300

    Fixed main Gui Issues with buttons and Window Layouts

--- MainProgramWindow.py
+++ MainProgramWindow.py
@@ -17,12 +17,14 @@ Virus = ['Human Papillomavirus (HPV)', 'Human Immunodeficiency Virus (HIV)',
          'Human T-lymphotropic Virus 1 (HTLV-1)', 'Human T-lymphotropic Virus 2 (HTLV-2)']
 functions = ['Retrovirus Infection Simulation',
              'Virus DNA Mutation', 'Virus Protein Transcription']
+# Theme Color for the Gui Application
 sg.theme('Dark')
 layout1 = [[sg.Text('Welcome to Virus Simulation App!!', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
            *[[sg.Radio(f' {Virus[i]}', 1)] for i in range(len(Virus))]]
 
 layout2 = [[sg.Text('Pick a virus simulation form the list to Begin.', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
            *[[sg.Radio(f' {functions[i]}', 2)] for i in range(len(functions))]]
+
 layout3 = [[sg.Text('Enter a new Virus Data to the Application:', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
            [sg.Input(key='-IN-'), sg.Button('Submit', key='-SubmitVName-')],
            [sg.Text('Enter a new Virus Mutation Rate to the Application:', size=(45, 1), font=(
@@ -32,7 +34,7 @@ layout3 = [[sg.Text('Enter a new Virus Data to the Application:', size=(45, 1), 
 
 # ----------- Create actual layout using Columns and a row of Buttons
 layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
-          [sg.Button('Choose Virus'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
+          [sg.Submit('Choose Virus', key='-MainButton-'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
 
 window = sg.Window('Virus Simulation Application', layout)
 
@@ -42,12 +44,22 @@ while True:
     print(event, values)
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
-    if event == 'Choose Virus':
-        window[f'-COL{layout}-'].update(visible=False)
-        layout = layout + 1 if layout < 3 else 1
-        window[f'-COL{layout}-'].update(visible=True)
+    if event == '-MainButton-':
+        window[f'-COL{1}-'].update(visible=False)
+        if layout == 1:
+            layout += 1
+            window[f'-COL{layout}-'].update(visible=True)
+            window['-MainButton-'].update('Choose Virus')
+        elif layout == 2:
+            layout = 1
+            window[f'-COL{layout+1}-'].update(visible=False)
+            window[f'-COL{layout}-'].update(visible=True)
+            window['-MainButton-'].update('Begin Simulation')
     elif event in '123':
         window[f'-COL{layout}-'].update(visible=False)
         layout = int(event)
         window[f'-COL{layout}-'].update(visible=True)
+        if layout == 3:
+            window['-MainButton-'].update(visible=False)
+
 window.close()
