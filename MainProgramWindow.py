5188086bf9ed24f8b19fd24e238033fa37f7b909
psi7 <pansid7@gmail.com>
2023-07-07 13:02:33 +0300

    Added button Initialized Function Implementation

--- MainProgramWindow.py
+++ MainProgramWindow.py
@@ -13,22 +13,26 @@ import PySimpleGUI as sg
 """
 
 # ----------- Create the 3 layouts this Window will display -----------
-Virus=['Human Papillomavirus (HPV)','Human Immunodeficiency Virus (HIV)','Human T-lymphotropic Virus 1 (HTLV-1)','Human T-lymphotropic Virus 2 (HTLV-2)']
-functions=['Retrovirus Infection Simulation','Virus DNA Mutation','Virus Protein Transcription']
-layout1 = [[sg.Text('Welcome to your Virus Simulation !!',size=(45,1),font=(16),background_color='white', text_color='black',justification='center',relief=sg.RELIEF_RAISED, enable_events=True)],
+Virus = ['Human Papillomavirus (HPV)', 'Human Immunodeficiency Virus (HIV)',
+         'Human T-lymphotropic Virus 1 (HTLV-1)', 'Human T-lymphotropic Virus 2 (HTLV-2)']
+functions = ['Retrovirus Infection Simulation',
+             'Virus DNA Mutation', 'Virus Protein Transcription']
+sg.theme('Dark')
+layout1 = [[sg.Text('Welcome to Virus Simulation App!!', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
            *[[sg.Radio(f' {Virus[i]}', 1)] for i in range(len(Virus))]]
 
-layout2 = [[sg.Text('Pick a virus simulation to Begin.')],
-           *[[sg.Radio(f' {functions[i]}',2)] for i in range(len(functions))]]
-layout3= [[sg.Text('Enter a new Virus to the Application:')],
-           [sg.Input(key='-IN-')],
-           [sg.Input(key='-IN2-')]]
-
+layout2 = [[sg.Text('Pick a virus simulation form the list to Begin.', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
+           *[[sg.Radio(f' {functions[i]}', 2)] for i in range(len(functions))]]
+layout3 = [[sg.Text('Enter a new Virus Data to the Application:', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
+           [sg.Input(key='-IN-'), sg.Button('Submit', key='-SubmitVName-')],
+           [sg.Text('Enter a new Virus Mutation Rate to the Application:', size=(45, 1), font=(
+               16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
+           [sg.Input(key='-IN2-'), sg.Button('Submit', key='-SubmitVMR-')]]
 
 
 # ----------- Create actual layout using Columns and a row of Buttons
 layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
-          [sg.Button('Cycle Layout'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
+          [sg.Button('Choose Virus'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
 
 window = sg.Window('Virus Simulation Application', layout)
 
@@ -38,7 +42,7 @@ while True:
     print(event, values)
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
-    if event == 'Cycle Layout':
+    if event == 'Choose Virus':
         window[f'-COL{layout}-'].update(visible=False)
         layout = layout + 1 if layout < 3 else 1
         window[f'-COL{layout}-'].update(visible=True)
