dbfaeec353659eb689974f0955cc8cdb3d3b1a9d
psi7 <pansid7@gmail.com>
2023-07-09 23:11:30 +0300

    Fixed functionality parsing between windows

--- MainProgramWindow.py
+++ MainProgramWindow.py
@@ -1,18 +1,9 @@
 import PySimpleGUI as sg
 # import PySimpleGUIQt as sg
 
-"""
-    Demo - Multiple layouts in a single window that are swapped in and out
-
-    If you've ever wanted to replace the contents of a window with another layout then perhaps
-    this demo is for you.  You cannot actually change the layout of a window dynamically, but what
-    you can do is make elements visible and invisible.  
-    
-    To "swap out" a portion of a window, use a Column element for that portion.  Add multiple Columns
-    on the same row and make only 1 of them active at a time
-"""
 
 # ----------- Create the 3 layouts this Window will display -----------
+icon = './media/virusapp.ico'
 Virus = ['Human Papillomavirus (HPV)', 'Human Immunodeficiency Virus (HIV)',
          'Human T-lymphotropic Virus 1 (HTLV-1)', 'Human T-lymphotropic Virus 2 (HTLV-2)']
 functions = ['Retrovirus Infection Simulation',
@@ -20,10 +11,10 @@ functions = ['Retrovirus Infection Simulation',
 # Theme Color for the Gui Application
 sg.theme('Dark')
 layout1 = [[sg.Text('Welcome to Virus Simulation App!!', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
-           *[[sg.Radio(f' {Virus[i]}', 1)] for i in range(len(Virus))]]
+           *[[sg.Radio(f' {Virus[i]}', 1, key=f'-VirusB{i+1}-',auto_size_text=True)] for i in range(len(Virus))]]
 
 layout2 = [[sg.Text('Pick a virus simulation form the list to Begin.', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
-           *[[sg.Radio(f' {functions[i]}', 2)] for i in range(len(functions))]]
+           *[[sg.Radio(f' {functions[i]}', 2,key=f'-Sim{i+1}-',auto_size_text=True)] for i in range(len(functions))]]
 
 layout3 = [[sg.Text('Enter a new Virus Data to the Application:', size=(45, 1), font=(16), background_color='white', text_color='black', justification='center', relief=sg.RELIEF_RAISED, enable_events=True)],
            [sg.Input(key='-IN-'), sg.Button('Submit', key='-SubmitVName-')],
@@ -36,7 +27,8 @@ layout3 = [[sg.Text('Enter a new Virus Data to the Application:', size=(45, 1), 
 layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
           [sg.Submit('Choose Virus', key='-MainButton-'), sg.Button('1', auto_size_button=True), sg.Button('2', auto_size_button=True), sg.Button('3', auto_size_button=True), sg.Button('Exit', auto_size_button=True)]]
 
-window = sg.Window('Virus Simulation Application', layout)
+window = sg.Window('Virus Simulation Application', layout,element_justification='center', element_padding=(5,5),resizable=True,default_element_size=(
+    40, 5),icon=icon)
 
 layout = 1  # The currently visible layout
 while True:
@@ -55,6 +47,22 @@ while True:
             window[f'-COL{layout+1}-'].update(visible=False)
             window[f'-COL{layout}-'].update(visible=True)
             window['-MainButton-'].update('Choose Virus')
+            if values['-Sim1-']:
+                Soption=functions[0]
+            elif values['-Sim2-']:
+                Soption = functions[1]
+            elif values['-Sim3-']:
+                Soption=functions[2]
+            print(Soption)
+        if values['-VirusB1-']:
+            option=Virus[0]
+        elif values['-VirusB2-']:
+            option = Virus[1]
+        elif values['-VirusB3-']:
+            option=Virus[2]
+        elif values['-VirusB4-']:
+            option=Virus[3]
+        print(option)
     elif event in '123':
         window[f'-COL{layout}-'].update(visible=False)
         layout = int(event)
